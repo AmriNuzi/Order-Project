cce341b6171be7849cf550a78844e78f90c7c939
AmriNuzi <amernuzi@gmail.com>
2025-02-14 13:16:26 +0100

    start project

---
+++ App.tsx
@@ -0,0 +1,24 @@
+// npx react-native run-androidimport { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Amri </Text>
+      <Button
+        title="kliko per tu bere G"
+        onPress={() => alert('Hello o mreza')}
+      />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
